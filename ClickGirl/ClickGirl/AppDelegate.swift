da5f157e045f7bc9d0350fe790f8ae3bc5540762
TakeoIshida <tkotoma@gmail.com>
2026-04-21 22:01:41 +0900

    auto: ファイル更新

--- ClickGirl/ClickGirl/AppDelegate.swift
+++ ClickGirl/ClickGirl/AppDelegate.swift
@@ -1,40 +1,33 @@
-//
-//  AppDelegate.swift
-//  ClickGirl
-//
-//  Created by 石田健雄 on 2026/03/08.
-//
-
 import UIKit
+import GoogleMobileAds
+import AppTrackingTransparency
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { _ in
+                DispatchQueue.main.async {
+                    MobileAds.shared.start { _ in
+                        NotificationCenter.default.post(name: .adMobReady, object: nil)
+                    }
+                }
+            }
+        } else {
+            MobileAds.shared.start { _ in
+                NotificationCenter.default.post(name: .adMobReady, object: nil)
+            }
+        }
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        GameManager.shared.saveGame()
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-
 }
-
