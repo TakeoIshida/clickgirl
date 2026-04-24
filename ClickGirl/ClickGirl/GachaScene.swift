af11455b5467fad0d7e9c57e7e9ee81d92ead656
TakeoIshida <tkotoma@gmail.com>
2026-04-24 21:30:53 +0900

    auto: ファイル更新

--- ClickGirl/ClickGirl/GachaScene.swift
+++ ClickGirl/ClickGirl/GachaScene.swift
@@ -975,9 +975,9 @@ class GachaScene: SKScene {
 
     // MARK: - カード裏面
 
-    private func makeCardBackNode(large: Bool, smallW: CGFloat = 58, smallH: CGFloat = 88) -> SKNode {
-        let w: CGFloat  = large ? 185 : smallW
-        let h: CGFloat  = large ? 260 : smallH
+    private func makeCardBackNode(large: Bool, smallW: CGFloat = 58, smallH: CGFloat = 88, largeW: CGFloat = 185, largeH: CGFloat = 260) -> SKNode {
+        let w: CGFloat  = large ? largeW : smallW
+        let h: CGFloat  = large ? largeH : smallH
         let fs: CGFloat = large ? 1.0 : smallW / 58
         let container   = SKNode()
 
