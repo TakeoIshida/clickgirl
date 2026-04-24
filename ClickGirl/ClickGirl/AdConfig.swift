41d827090a2a428a6a2d4ec4937fff10be201c52
TakeoIshida <tkotoma@gmail.com>
2026-04-24 21:58:37 +0900

    auto: ファイル更新

--- ClickGirl/ClickGirl/AdConfig.swift
+++ ClickGirl/ClickGirl/AdConfig.swift
@@ -10,8 +10,8 @@ enum AdConfig {
     static let appID          = "ca-app-pub-3940256099942544~1458002511"
     static let rewardedAdUnit = "ca-app-pub-3940256099942544/1712485313"
     #else
-    // 本番ID（AdMobコンソールで取得してここに入れること）
-    static let appID          = "YOUR_ADMOB_APP_ID"
-    static let rewardedAdUnit = "YOUR_REWARDED_AD_UNIT_ID"
+    // 本番ID
+    static let appID          = "ca-app-pub-8244563543256981~2074299497"
+    static let rewardedAdUnit = "ca-app-pub-8244563543256981/7350157388"
     #endif
 }
